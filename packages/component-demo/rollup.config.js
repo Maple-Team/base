63bb4a02c77446bce56ac2c6313f252be757c4e9
liutsing <liutsingluo@gmail.com>
2022-10-22 13:40:06 +0800

    build component step 3

--- packages/component-demo/rollup.config.js
+++ packages/component-demo/rollup.config.js
@@ -2,8 +2,10 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import dts from 'rollup-plugin-dts'
-
 import postcss from 'rollup-plugin-postcss'
+
+import { terser } from 'rollup-plugin-terser'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 
 const packageJson = require('./package.json')
 
@@ -22,7 +24,15 @@ export default [
         sourcemap: true,
       },
     ],
-    plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), postcss()],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      postcss(),
+
+      terser(),
+    ],
   },
   {
     input: 'dist/esm/index.d.ts',
