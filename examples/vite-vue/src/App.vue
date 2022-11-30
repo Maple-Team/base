0a01c65a822d0b5884fdc839a9a75bcbcaaf8387
Liutsing <liutsingluo@163.com>
2022-11-30 22:21:40 +0800

    feat: ripple button example

--- examples/vite-vue/src/App.vue
+++ examples/vite-vue/src/App.vue
@@ -1,46 +1,12 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-// const unusedVar = '234234'
+import { RippleButton } from '@liutsing/vue-components'
+const handleClick = (e: MouseEvent) => {
+  console.log(e, 'click')
+}
 </script>
 
 <template>
-  <div>
-    <a
-      href="https://vitejs.dev"
-      target="_blank"
-    >
-      <img
-        src="/vite.svg"
-        class="logo"
-        alt="Vite logo"
-      />
-    </a>
-    <a
-      href="https://vuejs.org/"
-      target="_blank"
-    >
-      <img
-        src="./assets/vue.svg"
-        class="logo vue"
-        alt="Vue logo"
-      />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <RippleButton @click="handleClick">Find More</RippleButton>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
