e0b8fea9f21e2f9465a49b4901caacce606c4941
Liutsing <liutsingluo@163.com>
2022-12-25 21:30:43 +0800

    react component example

--- examples/vite-react/src/App.tsx
+++ examples/vite-react/src/App.tsx
@@ -1,9 +1,8 @@
 import React, { useCallback, useState } from 'react'
 import '@liutsing/rc-components/dist/esm/index.css'
-import { RenderComponent } from './RenderComponent'
-import { RippleButton } from '@liutsing/rc-components'
+import { ListWithMore } from './List'
 
-const UseCount = () => {
+export const UseCount = () => {
   const [num, setNum] = useState<number>(0)
   const increase = useCallback(() => {
     setNum((_) => _ + 1)
@@ -17,7 +16,7 @@ const UseCount = () => {
     decrease,
   }
 }
-const CounterRenderProps = ({
+export const CounterRenderProps = ({
   children, // children特殊属性
 }: {
   children: ({ num, increase, decrease }: { num: number; increase: () => void; decrease: () => void }) => JSX.Element
@@ -27,12 +26,19 @@ const CounterRenderProps = ({
 }
 
 const App = () => {
-  const { num, increase, decrease } = UseCount()
+  // const { num, increase, decrease } = UseCount()
+  const [data, _setData] = useState<number[]>([1, 2, 3, 4, 5, 6, 7, 8, 9])
+
   return (
     <>
-      <RenderComponent render={() => <div>render function component</div>} />
+      <ListWithMore
+        data={data}
+        max={5}
+        renderItem={(item) => <span key={item}>{item}</span>}
+      />
+      {/* <RenderComponent render={() => <div>render function component</div>} /> */}
       {/* <Counter /> */}
-      <div>
+      {/* <div>
         <span>num: {num}</span>
         <RippleButton onClick={increase}>+</RippleButton>
         <RippleButton onClick={decrease}>-</RippleButton>
@@ -47,10 +53,10 @@ const App = () => {
             </>
           )
         }}
-      </CounterRenderProps>
-      <footer>
+      </CounterRenderProps> */}
+      {/* <footer>
         <a href="https://skk.moe/">Sukka</a>
-      </footer>
+      </footer> */}
     </>
   )
 }
--- examples/vite-react/src/main.tsx
+++ examples/vite-react/src/main.tsx
@@ -5,11 +5,12 @@ import 'antd/dist/reset.css'
 import { ConfigProvider } from '@liutsing/rc-components'
 // @ts-ignore
 import { App as AnAPP } from '@liutsing/rcv-components'
+import './style.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ConfigProvider prefixCls="tee">
-      <AnAPP />
+      {/* <AnAPP /> */}
       <App />
     </ConfigProvider>
   </React.StrictMode>
