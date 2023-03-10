d24a822c176ac3b837bb2236c57162283d4bcdf5
liutsing <liutsingluo@gmail.com>
2023-03-10 23:23:23 +0800

    ~

--- packages/react-components/src/components/scrollbar/index.tsx
+++ packages/react-components/src/components/scrollbar/index.tsx
@@ -1,6 +1,36 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react'
+/**
+ * 滚动处理
+ * reference:  @https://www.thisdot.co/blog/creating-custom-scrollbars-with-react
+ * @Author: liutsing
+ * @Date: 2023-03-10 Friday 15:50
+ */
+import React, { useState, useEffect, useRef, useCallback, ReactNode } from 'react'
 
-export const Scrollbar = ({ children, ...props }: React.ComponentPropsWithoutRef<'div'>) => {
+type Direction = 'up' | 'down'
+interface DirectionButtonPros {
+  children?: ReactNode
+  direction: Direction
+  onClick: (direction: Direction) => void
+}
+
+const DirectionButton = ({ onClick, direction, children }: DirectionButtonPros) => {
+  const _onClick = useCallback(() => onClick(direction), [direction])
+
+  return (
+    <button
+      className="custom-scrollbars__button"
+      onClick={_onClick}
+    >
+      {children ?? direction === 'down' ? '⇓' : '⇑'}
+    </button>
+  )
+}
+
+export type ScrollbarProps = React.ComponentPropsWithoutRef<'div'> & {
+  showDirectionButton?: boolean
+}
+
+export const Scrollbar = ({ children, showDirectionButton, ...props }: ScrollbarProps) => {
   const contentRef = useRef<HTMLDivElement>(null)
   // track轨道
   const scrollTrackRef = useRef<HTMLDivElement>(null)
@@ -27,16 +57,16 @@ export const Scrollbar = ({ children, ...props }: React.ComponentPropsWithoutRef
   }
 
   /**
-   * browser compatiable
+   * browser compatible
    * @param direction
    */
-  function handleScrollButton(direction: 'up' | 'down') {
+  const onDirectionButtonClick = useCallback((direction: Direction) => {
     const { current } = contentRef
     if (current) {
       const scrollAmount = direction === 'down' ? 200 : -200
       current.scrollBy({ top: scrollAmount, behavior: 'smooth' })
     }
-  }
+  }, [])
 
   const handleTrackClick = useCallback(
     (e: React.MouseEvent<HTMLDivElement>) => {
@@ -73,32 +103,28 @@ export const Scrollbar = ({ children, ...props }: React.ComponentPropsWithoutRef
     thumb.style.top = `${newTop}px`
   }, [])
 
-  const handleThumbMousedown = useCallback(
-    (e: { preventDefault: () => void; stopPropagation: () => void; clientY: React.SetStateAction<number | null> }) => {
-      e.preventDefault()
-      e.stopPropagation()
-      setScrollStartPosition(e.clientY)
-      if (contentRef.current) setInitialScrollTop(contentRef.current.scrollTop)
-      setIsDragging(true)
-    },
-    []
-  )
-
-  const handleThumbMouseup = useCallback(
-    (e: { preventDefault: () => void; stopPropagation: () => void }) => {
-      e.preventDefault()
-      e.stopPropagation()
-      if (isDragging) {
-        setIsDragging(false)
-      }
-    },
-    [isDragging]
-  )
+  // If the content and the scrollbar track exist,
+  // use a ResizeObserver to adjust height of thumb and listen for scroll event to move the thumb
+  // 如果内容和滚动条轨道存在，则使用ResizeObserver来调整滑块的高度，并侦听滚动事件以移动滑块。
+  useEffect(() => {
+    if (!(contentRef.current && scrollTrackRef.current)) {
+      return
+    }
+    const ref = contentRef.current
+    const { clientHeight: trackSize } = scrollTrackRef.current
+    observer.current = new ResizeObserver(() => {
+      handleResize(ref, trackSize)
+    })
+    observer.current.observe(ref)
+    ref.addEventListener('scroll', handleThumbPosition)
+    return () => {
+      observer.current?.unobserve(ref)
+      ref.removeEventListener('scroll', handleThumbPosition)
+    }
+  }, [])
 
   const handleThumbMousemove = useCallback(
-    (e: { preventDefault: () => void; stopPropagation: () => void; clientY: number }) => {
-      e.preventDefault()
-      e.stopPropagation()
+    (e: MouseEvent) => {
       if (isDragging) {
         if (contentRef.current) {
           const { scrollHeight: contentScrollHeight, offsetHeight: contentOffsetHeight } = contentRef.current
@@ -112,24 +138,23 @@ export const Scrollbar = ({ children, ...props }: React.ComponentPropsWithoutRef
     [isDragging, scrollStartPosition, thumbHeight]
   )
 
-  // If the content and the scrollbar track exist, use a ResizeObserver to adjust height of thumb and listen for scroll event to move the thumb
-  useEffect(() => {
-    if (contentRef.current && scrollTrackRef.current) {
-      const ref = contentRef.current
-      const { clientHeight: trackSize } = scrollTrackRef.current
-      observer.current = new ResizeObserver(() => {
-        handleResize(ref, trackSize)
-      })
-      observer.current.observe(ref)
-      ref.addEventListener('scroll', handleThumbPosition)
-      return () => {
-        observer.current?.unobserve(ref)
-        ref.removeEventListener('scroll', handleThumbPosition)
-      }
-    }
+  const handleThumbMousedown = useCallback((e: React.MouseEvent) => {
+    setScrollStartPosition(e.clientY)
+    if (contentRef.current) setInitialScrollTop(contentRef.current.scrollTop)
+    setIsDragging(true)
   }, [])
 
+  const handleThumbMouseup = useCallback(
+    (e: MouseEvent) => {
+      e.stopPropagation()
+      if (isDragging) {
+        setIsDragging(false)
+      }
+    },
+    [isDragging]
+  )
   // Listen for mouse events to handle scrolling by dragging the thumb
+  // 拖动滑块时动态监听事件
   useEffect(() => {
     document.addEventListener('mousemove', handleThumbMousemove)
     document.addEventListener('mouseup', handleThumbMouseup)
@@ -151,12 +176,12 @@ export const Scrollbar = ({ children, ...props }: React.ComponentPropsWithoutRef
         {children}
       </div>
       <div className="custom-scrollbars__scrollbar">
-        <button
-          className="custom-scrollbars__button"
-          onClick={() => handleScrollButton('up')}
-        >
-          ⇑
-        </button>
+        {showDirectionButton && (
+          <DirectionButton
+            direction="up"
+            onClick={onDirectionButtonClick}
+          />
+        )}
         <div className="custom-scrollbars__track-and-thumb">
           <div
             className="custom-scrollbars__track"
@@ -174,12 +199,12 @@ export const Scrollbar = ({ children, ...props }: React.ComponentPropsWithoutRef
             }}
           />
         </div>
-        <button
-          className="custom-scrollbars__button"
-          onClick={() => handleScrollButton('down')}
-        >
-          ⇓
-        </button>
+        {showDirectionButton && (
+          <DirectionButton
+            direction="down"
+            onClick={onDirectionButtonClick}
+          />
+        )}
       </div>
     </div>
   )
