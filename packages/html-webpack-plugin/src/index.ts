0449daa42a33dcb925c912a71e5d19a9ed12ff70
Liutsing <liutsingluo@163.com>
2023-03-18 01:37:10 +0800

    feat: add link inject logic

--- packages/html-webpack-plugin/src/index.ts
+++ packages/html-webpack-plugin/src/index.ts
@@ -1,20 +1,24 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import * as webpack from 'webpack'
 
+type LinkAttribute = Pick<HTMLLinkElement, 'rel' | 'href'>
+type ScriptAttribute = Pick<HTMLScriptElement, 'src'>
+
 // TODO 支持更多注入的
-export interface Options {
-  src?: string
+export type Options = {
   content?: string
-  tagName: 'script'
-}
+  tagName: 'script' | 'link'
+} & LinkAttribute &
+  ScriptAttribute
 export default class MapleHtmlWebpackPlugin {
   /**
    * @type {Options}
    */
   private options
-  // TODO 类型处理
-  constructor(options?: Options | Options[]) {
+  private position
+  constructor(options: Options | Options[], position?: 'header' | 'body') {
     this.options = options
+    this.position = position || 'body'
   }
 
   apply(compiler: webpack.Compiler) {
@@ -24,18 +28,32 @@ export default class MapleHtmlWebpackPlugin {
           if (!Array.isArray(this.options)) {
             this.options = [this.options]
           }
-          const tags = this.options.map(({ tagName, src, content }) => ({
-            tagName,
-            voidTag: false,
-            attributes: {
-              src,
-              type: 'application/javascript',
-            },
-            innerHTML: content,
-            meta: {},
-          }))
-
-          data.headTags.unshift(...tags)
+          const tags = this.options.map(({ tagName, src, content, rel, href }) => {
+            return tagName === 'script'
+              ? {
+                  tagName,
+                  voidTag: false,
+                  attributes: {
+                    src,
+                  },
+                  innerHTML: content,
+                  meta: {},
+                }
+              : {
+                  tagName,
+                  voidTag: true,
+                  attributes: {
+                    rel,
+                    href,
+                  },
+                  meta: {},
+                }
+          })
+          if (this.position === 'header') {
+            data.headTags.push(...tags)
+          } else {
+            data.bodyTags.push(...tags)
+          }
         }
         cb(null, data)
       })
