1bbb7f1e1b42b2e09a35e2cb604bf4cf6011c3b1
liutsing <liutsingluo@gmail.com>
2023-05-17 11:30:39 +0800

    debug: react-query请求测试通过

--- examples/webpack-react/__tests__/app.test.tsx
+++ examples/webpack-react/__tests__/app.test.tsx
@@ -1,56 +1,59 @@
-import nock from 'nock'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import type { ReactNode } from 'react'
 import React from 'react'
 import { renderHook, waitFor } from '@testing-library/react'
-import mockData from '@/mockData.json'
-import { baseURL, fetchApiInfo, useCustomHook } from '@/pages/ReactQueryTest'
+import { useCustomHook } from '@/pages/ReactQueryTest'
 
-describe('react query test case', () => {
+const createWrapper = () => {
   const queryClient = new QueryClient({
+    logger: {
+      log: console.log,
+      warn: console.warn,
+      error: () => {},
+    },
     defaultOptions: {
       queries: {
-        retry: false,
+        retry: false, // 测试react-query首先我们必须得拥有queryClient，所以我们初始化queryClient，
+        // 因为默认是重试三次，这意味着如果想测试错误的查询，测试可能会超时。所以可以在初始化时关闭
       },
     },
   })
-  const wrapper = ({ children }: { children: ReactNode }) => (
+  return ({ children }: { children: ReactNode }) => (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   )
-  beforeEach(() => {
-    nock.restore()
+}
 
-    nock(baseURL, {
-      reqheaders: {
-        Origin: 'http://localhost',
-      },
-    })
-      .get('/api')
-      .query(true)
-      .reply(200, mockData, {
-        'Access-Control-Allow-Origin': 'http://localhost',
-      })
-  })
+describe('react query test case', () => {
+  // beforeEach(() => {
+  //   nock.restore()
 
-  afterEach(() => {
-    // nock.restore()
-    nock.cleanAll()
-    // nock.enableNetConnect()
-    // nock.emitter.removeAllListeners()
-    // nock.activate()
-    // nock.enableNetConnect()
-  })
+  //   nock(baseURL, {
+  //     reqheaders: {
+  //       Origin: 'http://localhost',
+  //     },
+  //   })
+  //     .get('/api')
+  //     .query(true)
+  //     .reply(200, mockData, {
+  //       'Access-Control-Allow-Origin': 'http://localhost',
+  //     })
+  // })
+
+  // afterEach(() => {
+  //   // nock.restore()
+  //   nock.cleanAll()
+  //   // nock.enableNetConnect()
+  //   // nock.emitter.removeAllListeners()
+  //   // nock.activate()
+  //   // nock.enableNetConnect()
+  // })
 
   it('CustomHook test', async () => {
-    const res = await fetchApiInfo()
-
-    expect(res).toStrictEqual(mockData)
-
-    const { result } = renderHook(() => useCustomHook(), { wrapper })
-
+    const { result } = renderHook(() => useCustomHook(), { wrapper: createWrapper() })
     expect(result.current.isLoading).toBe(true)
-    await waitFor(() => result.current.isSuccess)
-    console.log(result.current.data, 'data', result.current.isLoading, result.current.isSuccess)
-    // expect(result.current.data).toEqual(mockData)
+    await waitFor(() => expect(result.current.isSuccess).toBe(true))
+    expect(result.current.data).toBeDefined()
+    console.log(result.current.data, result.current.isLoading, result.current.isSuccess)
+    expect(result.current.data).toBeDefined()
   })
 })
--- examples/webpack-react/src/pages/ReactQueryTest.tsx
+++ examples/webpack-react/src/pages/ReactQueryTest.tsx
@@ -1,20 +1,18 @@
 import { useQuery } from '@tanstack/react-query'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import dayjs from 'dayjs'
 import type { Root } from './type'
 
 export const baseURL = 'https://randomuser.me'
 export const axiosInstance = axios.create({
-  baseURL,
   timeout: 5 * 60 * 1000,
 })
 axiosInstance.defaults.baseURL = baseURL
 
-export const fetchApiInfo = (): Promise<Root> => axiosInstance.get('/api').then((res) => res.data)
+const fetchApiInfo = (): Promise<Root> => axiosInstance.get('/api').then((res) => res.data)
 
 export function useCustomHook() {
-  return useQuery(['customHook', { t: dayjs().valueOf() }], fetchApiInfo)
+  return useQuery(['customHook'], fetchApiInfo)
 }
 
 export const useFetchInfo = () => {
