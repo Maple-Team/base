74adbe38e601e708040e6bfbd18b50b6b2fc6cb4
liutsing <liutsingluo@gmail.com>
2023-05-17 15:23:46 +0800

    test: 元素内容异步变更测试

--- examples/webpack-react/__tests__/app.test.tsx
+++ examples/webpack-react/__tests__/app.test.tsx
@@ -1,11 +1,11 @@
-import { act, renderHook, waitFor } from '@testing-library/react'
+import { act, renderHook, waitFor, screen } from '@testing-library/react'
 import { rest } from 'msw'
 import React from 'react'
 import { server } from '../setupTests'
 import { createWrapper, renderWithClient } from './utils'
 import mockData from '@/mockData.json'
 import { useCustomHook } from '@/hooks'
-import Example from '@/pages/ReactQueryTest'
+import { Example1, Example2 } from '@/pages/ReactQueryTest'
 
 describe('react query test case', () => {
   it('CustomHook test', async () => {
@@ -31,27 +31,48 @@ describe('react query test case', () => {
     expect(result.current.error).toBeDefined()
     expect(result.current.data).toBeUndefined()
   })
-  it('successful query component', async () => {
-    console.time('label')
-    const result = renderWithClient(<Example />)
 
-    // NOTE case 1: 元素一开始就存在，需要等待
-    const target = result.getByTestId('test-id')
-    expect(target).toHaveTextContent('')
+  // working, but warning act not setup right
+  // it('successful query component when component initial exist case 1', async () => {
+  //   const result = renderWithClient(<Example2 />)
+  //   const target = result.getByTestId('test-id')
+  //   expect(target).toHaveTextContent('')
+
+  //   await waitFor(() => {
+  //     const element = result.getByTestId('test-id')
+  //     return element.innerHTML === mockData.info.seed
+  //   })
+  //   await act(async () => {
+  //     await waitFor(() => {})
+  //   })
+
+  //   expect(target).toHaveTextContent(mockData.info.seed)
+  // })
+
+  it('successful query component when component initial exist case 2', async () => {
+    await act(() => {
+      renderWithClient(<Example2 />)
+    })
+
+    expect(screen.getByTestId('test-id')).toHaveTextContent('')
 
     await waitFor(() => {
-      const element = result.getByTestId('test-id')
-      return element.innerHTML === mockData.info.seed
+      const element = screen.getByTestId('test-id')
+      return element.innerHTML !== ''
     })
 
-    await act(async () => {
-      await waitFor(() => {})
+    await waitFor(() => {
+      const target = screen.getByTestId('test-id')
+      return target.innerHTML === mockData.info.seed
     })
+    const target = screen.getByTestId('test-id')
+    expect(target).toHaveTextContent(mockData.info.seed)
+  })
 
-    expect(result.getByTestId('test-id')).toHaveTextContent(mockData.info.seed)
-
+  it('successful query component when component initial not exist', async () => {
+    const result = renderWithClient(<Example1 />)
     // NOTE case 2: 使用异步查询: 元素一开始不存在
-    // const target = await result.findByTestId('test-id')
-    // expect(target).toHaveTextContent(mockData.info.seed)
+    const target = await result.findByTestId('test-id')
+    expect(target).toHaveTextContent(mockData.info.seed)
   })
 })
--- examples/webpack-react/src/pages/ReactQueryTest.tsx
+++ examples/webpack-react/src/pages/ReactQueryTest.tsx
@@ -11,13 +11,13 @@ export const useFetchInfo = () => {
 
   return seed
 }
-// export default () => {
-//   const { isLoading, data } = useCustomHook()
-//   if (isLoading) return <div>Loading...</div>
-//   return <div data-testid="test-id">{data?.info.seed}</div>
-// }
 
-export default () => {
+export const Example2 = () => {
   const seed = useFetchInfo()
   return <div data-testid="test-id">{seed}</div>
+}
+export const Example1 = () => {
+  const { isLoading, data } = useCustomHook()
+  if (isLoading) return <div>Loading...</div>
+  return <div data-testid="test-id">{data?.info.seed}</div>
 }
