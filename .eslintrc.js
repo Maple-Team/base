d876ca9c25141a60e7a35e74e0b2c8fc7e9346e9
liutsing <liutsingluo@gmail.com>
2023-06-17 16:45:55 +0800

    debug: gh-pages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: '@liutsing/eslint-config',
-  ignorePatterns: ['*.json'],
+  ignorePatterns: ['*.json', 'docs'],
   rules: {
     'multiline-ternary': 'off',
     '@typescript-eslint/restrict-template-expressions': 'off',
