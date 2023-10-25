dbd5cc36a975c5e1c02ff473283bea01e4ed467b
Liutsing Robert <liutsingluo@gmail.com>
2023-10-25 10:44:21 +0800

    feat: 正确处理html-webpack-plugin

--- examples/webpack-react/config/webpack.dev.js
+++ examples/webpack-react/config/webpack.dev.js
@@ -1,46 +1,73 @@
 const path = require('path')
 const os = require('os')
 const { dev } = require('@liutsing/webpack-config')
-const { merge } = require('webpack-merge')
+const {
+  // mergeWithCustomize,
+  // unique,
+  merge,
+} = require('webpack-merge')
 const FontMinifyPlugin = require('@liutsing/font-minify-plugin')
-const LifeCycleWebpackPlugin = require('@liutsing/lifecycle-webpack-plugin')
+const webpack = require('webpack')
+const MapleHtmlWebpackPlugin = require('@liutsing/html-webpack-plugin')
 
-const config = merge(dev, {
-  entry: path.resolve(__dirname, '../src/main.tsx'),
-  mode: 'development',
-  resolve: {
-    alias: {
-      react: path.resolve(__dirname, '../../../node_modules/react'),
-      'react-dom': path.resolve(__dirname, '../../../node_modules/react-dom'),
-    },
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
-  },
-  watchOptions: {
-    ignored: ['**/public/fonts', '**/node_modules'],
-  },
-  plugins: [
-    new FontMinifyPlugin({
-      words: path.resolve(os.tmpdir(), 'example-webpack-react.txt'),
-      fontSource: path.resolve(__dirname, '../../../packages/font-minify-plugin/puhui2/AlibabaPuHuiTi_2_65_Medium.ttf'),
-      fontDistFilename: 'AlibabaPuHuiTi_2_65_Medium',
-    }),
-    new FontMinifyPlugin({
-      words: '魑魅魍魉',
-      isFilePath: false,
-    }),
-    new LifeCycleWebpackPlugin({
-      compile: () => {
-        console.log('\n compile \n', new Date())
+// const LifeCycleWebpackPlugin = require('@liutsing/lifecycle-webpack-plugin')
+// const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
+
+// const smp = new SpeedMeasurePlugin()
+
+const config =
+  // mergeWithCustomize({
+  //   customizeArray: unique('plugins', ['HtmlWebpackPlugin'], (plugin) => plugin.constructor && plugin.constructor.name),
+  // })
+
+  merge(dev, {
+    entry: path.resolve(__dirname, '../src/main.tsx'),
+    mode: 'development',
+    resolve: {
+      alias: {
+        react: path.resolve(__dirname, '../../../node_modules/react'),
+        'react-dom': path.resolve(__dirname, '../../../node_modules/react-dom'),
       },
-      done: () => {
-        console.log('\n done \n', new Date())
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
       },
-    }),
-  ],
-})
+    },
+    watchOptions: {
+      ignored: ['**/public/fonts', '**/node_modules'],
+    },
+    plugins: [
+      new webpack.DllReferencePlugin({
+        context: path.resolve(__dirname, '../'),
+        manifest: path.join(__dirname, '../public', 'vendor-manifest.json'),
+      }),
+
+      new MapleHtmlWebpackPlugin([{ tagName: 'script', src: 'vendor.bundle.js', defer: true }], 'head'),
+
+      new FontMinifyPlugin({
+        words: path.resolve(os.tmpdir(), 'example-webpack-react.txt'),
+        fontSource: path.resolve(
+          __dirname,
+          '../../../packages/font-minify-plugin/puhui2/AlibabaPuHuiTi_2_65_Medium.ttf'
+        ),
+        fontDistFilename: 'AlibabaPuHuiTi_2_65_Medium',
+      }),
+      new FontMinifyPlugin({
+        words: '魑魅魍魉',
+        isFilePath: false,
+      }),
+      // new LifeCycleWebpackPlugin({
+      //   compile: () => {
+      //     console.log('\n compile \n', new Date())
+      //   },
+      //   done: () => {
+      //     console.log('\n done \n', new Date())
+      //   },
+      // }),
+    ],
+  })
+
+// module.exports = smp.wrap(config)
 
 module.exports = config
--- packages/webpack-config/config/webpack.base.js
+++ packages/webpack-config/config/webpack.base.js
@@ -1,31 +1,43 @@
 const path = require('path')
+const child = require('child_process')
 const { ProvidePlugin, DefinePlugin } = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MapleHtmlWebpackPlugin = require('@liutsing/html-webpack-plugin')
-const SpriteLoaderPlugin = require('svg-sprite-loader/plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
+const dayjs = require('dayjs')
+const MapleHtmlWebpackPlugin = require('@liutsing/html-webpack-plugin')
+
+const currentGitBranch = child.execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
+const hash = child.execSync('git rev-parse HEAD').toString().trim().substring(0, 8)
 
 // TODO 参考vue-cli/cra/其他开源社区分享
 
 const root = process.cwd()
 const mode = process.env.NODE_ENV
-const isDev = mode === 'production'
+const isDev = mode === 'development'
 const envKeys = require('../plugins/env.js')(root)
 
+const { version } = require(path.resolve(root, 'package.json'))
 /**
  * @type {import('webpack').Configuration}
  */
 const config = {
   entry: path.resolve(root, './src/main.tsx'),
+  output: {
+    path: path.resolve(root, './dist'),
+    filename: '[name].[contenthash].js',
+    chunkFilename: '[name].[contenthash].js',
+    publicPath: '/',
+    clean: true,
+  },
   resolve: {
-    extensions: ['.js', '.ts', '.tsx', '.jsx', '.node', '.wasm'],
-    mainFiles: ['index'],
+    extensions: ['.js', '.ts', '.tsx', '.jsx', '.node', '.wasm', '.css', '.less', '.scss', '.styl'],
     alias: {
       '@': path.resolve(root, './src'),
     },
   },
   target: 'web',
+  cache: isDev,
   module: {
     rules: [
       {
@@ -57,23 +69,121 @@ const config = {
         sideEffects: true,
       },
       {
-        // FIXME
         test: /\.svg$/,
         exclude: /node_modules/,
-        use: [
-          'svg-sprite-loader',
-          // 'svgo-loader',  Node.js tool for optimizing SVG files @https://github.com/svg/svgo
-        ],
+        use: ['@svgr/webpack', 'svgo-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       inject: true,
-      template: 'config/index.html',
-      title: 'webpack config demo',
+      hash: true,
+      cache: false,
+      templateContent: () => `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <title>webpack example</title> 
+      </head>
+      <body>
+        <!-- // TODO LOADING 主题变量-->
+        <div id="app"></div>
+      </body> 
+    </html>
+      `,
+      meta: {
+        viewport: 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no',
+        a: {
+          'http-Equiv': 'Content-Type',
+          content: 'text/html; charset=utf-8',
+        },
+        b: {
+          'http-equiv': 'X-UA-Compatible',
+          content: 'IE=edge',
+        },
+        c: {
+          'http-equiv': 'expires',
+          content: 'Wed, 26 Feb 1997 08:21:57 GMT',
+        },
+        d: {
+          'http-equiv': 'pragma',
+          content: 'no-cache',
+        },
+        e: {
+          'http-equiv': 'Cache-Control',
+          content: 'no-store, must-revalidate',
+        },
+      },
     }),
-    new MapleHtmlWebpackPlugin({ tagName: 'link', rel: 'stylesheet', href: '/fonts.css' }, 'head'),
+    new MapleHtmlWebpackPlugin(
+      [
+        {
+          tagName: 'script',
+          content: `;(function () {
+          window.appHash = '${hash}';
+          window.appBranch = '${currentGitBranch}';
+          var c = '#41b883',
+            f = '#42c02e',
+            l = function (t) {
+              var e = t.title,
+                r = t.content,
+                n = t.backgroundColor,
+                i = [
+                  '%c '.concat(e, ' %c ').concat(r, ' '),
+                  'padding: 1px; border-radius: 3px 0 0 3px; color: #fff; background: '.concat('#35495e', ';'),
+                  'padding: 1px; border-radius: 0 3px 3px 0; color: #fff; background: '.concat(n, ';'),
+                ]
+              return (
+                function () {
+                  var t
+                  console && 'function' === typeof console.log && (t = console).log.apply(t, arguments)
+                }.apply(void 0, i),
+                i
+              )
+            }
+
+          function p(t) {
+            var e = t.title,
+              r = t.content
+            return l({
+              title: e,
+              content: r,
+              backgroundColor: c,
+            })
+          }
+          p({
+            title: 'Build Date',
+            content: '${dayjs().format('YYYY-MM-DD HH:mm:ss')}',
+          })
+          p({
+            title: 'Build Version',
+            content: '${version}',
+          })
+          p({
+            title: 'Build Commit',
+            content: '${hash}',
+          })
+          p({
+            title: 'Build Branch',
+            content: '${currentGitBranch}',
+          })
+        })()`,
+        },
+        {
+          content: `
+        window.addEventListener('error', function handleError(e) {
+          // prompt user to confirm refresh
+          console.debug(e)
+          if (/Loading (?:CSS\\s)?chunk \\d+ failed/.test(e.message)) {
+            window.location.reload();
+          }
+        })`,
+          tagName: 'script',
+        },
+      ],
+      'body'
+    ),
     new CopyPlugin({
       patterns: [
         {
@@ -89,17 +199,9 @@ const config = {
     new DefinePlugin({
       ...envKeys,
     }),
-    new SpriteLoaderPlugin({
-      plainSprite: true,
-    }),
+
     !isDev ? new MiniCssExtractPlugin() : null,
   ].filter(Boolean),
-  output: {
-    filename: '[name].[contenthash].js',
-    chunkFilename: '[name].[contenthash].js',
-    publicPath: '/',
-    clean: true,
-  },
 }
 
 module.exports = config
