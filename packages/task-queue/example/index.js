36585f415feb72eb2f8bf222b8537b5fdbbbe53a
Liutsing Robert <liutsingluo@gmail.com>
2024-01-03 17:43:01 +0800

    feat: 更新示例代码

--- packages/task-queue/example/index.js
+++ packages/task-queue/example/index.js
@@ -1,35 +1,52 @@
+const { sleep } = require('@liutsing/utils')
 const TaskQueue = require('../')
 
-const taskQueue = new TaskQueue({
-  async taskCommand() {
-    return Promise.resolve()
-  },
-  autoStart: false,
-  wait: true,
-  interval: 100,
-})
+// eslint-disable-next-line import/newline-after-import
+;(async () => {
+  /**
+   * @type {import('../').default}
+   */
+  const taskQueue = new TaskQueue({
+    async taskCommand() {
+      await sleep(1 * 1000)
+      return Promise.resolve()
+    },
+    autoStart: false,
+    wait: true,
+    interval: 1000,
+  })
 
-taskQueue.addQueue('1')
-taskQueue.addQueue('2')
-taskQueue.addQueue('3')
-taskQueue.start()
-taskQueue.addQueue('4')
-taskQueue.addQueue('5')
-taskQueue.addQueue('6')
+  taskQueue.addQueue('1')
+  taskQueue.addQueue('2')
+  taskQueue.addQueue('3')
+  console.log(new Date())
 
-const taskQueue2 = new TaskQueue({
-  async taskCommand() {
-    return Promise.resolve()
-  },
-  autoStart: false,
-  wait: true,
-  interval: 100,
-})
+  taskQueue.start()
+  await sleep(1 * 1000)
 
-taskQueue2.addQueue('21')
-taskQueue2.addQueue('22')
-taskQueue2.addQueue('23')
-taskQueue2.start()
-taskQueue2.addQueue('24')
-taskQueue2.addQueue('25')
-taskQueue2.addQueue('26')
+  taskQueue.addQueue('4')
+  taskQueue.addQueue('5')
+  taskQueue.addQueue('6')
+  // 判断此时是否还有需要执行的任务
+  if (taskQueue.info().waitExecute !== 0) taskQueue.start()
+
+  console.log(new Date())
+
+  // const taskQueue2 = new TaskQueue({
+  //   async taskCommand() {
+  //     return Promise.resolve()
+  //   },
+  //   autoStart: false,
+  //   wait: true,
+  //   interval: 100,
+  // })
+
+  // taskQueue2.addQueue('21')
+  // taskQueue2.addQueue('22')
+  // taskQueue2.addQueue('23')
+
+  // taskQueue2.addQueue('24')
+  // taskQueue2.addQueue('25')
+  // taskQueue2.addQueue('26')
+  // taskQueue2.start()
+})()
