f79ea8b5ece26612de481de791e828bda92a6b76
Liutsing Robert <liutsingluo@gmail.com>
2024-06-03 14:27:04 +0800

    chore: ~

--- packages/nodejs-utils/src/hash/md5.ts
+++ packages/nodejs-utils/src/hash/md5.ts
@@ -1,7 +1,6 @@
 import * as crypto from 'crypto'
 import * as fs from 'fs'
 import { Buffer } from 'buffer'
-import type { HashAlgorithm } from './hash'
 
 const BUFFER_SIZE = 8192
 
@@ -38,29 +37,4 @@ export function md5File(path: fs.PathLike) {
 
     input.pipe(output)
   })
-}
-
-export function hashFile(path: fs.PathLike, algo: HashAlgorithm) {
-  const hash = crypto.createHash
-  return new Promise<String>((resolve, reject) => {
-    fs.readFile(path, (err, data) => {
-      if (err) reject(err)
-
-      resolve(hash(algo).update(data).digest('base64'))
-    })
-  })
-}
-export function hashFileSync(path: fs.PathLike, algo: HashAlgorithm) {
-  const hash = crypto.createHash
-  const content = fs.readFileSync(path)
-  return hash(algo).update(content).digest('base64')
-}
-
-/**
- * hash处理字符串
- * @param key
- * @returns
- */
-export function hash(key: string, algorithm: HashAlgorithm = 'sha1') {
-  return crypto.createHash(algorithm).update(`${key}`).digest('hex')
 }
