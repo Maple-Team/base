944229cdc87fcbfb7cc29cce03d44c9826fa35b2
Liutsing Robert <liutsingluo@gmail.com>
2024-10-24 00:01:58 +0800

    feat: 简易版axios-retry

--- examples/webpack-react/src/http/axios.ts
+++ examples/webpack-react/src/http/axios.ts
@@ -1,7 +1,51 @@
-import type { AxiosResponse } from 'axios'
+import type { AxiosError, AxiosResponse } from 'axios'
 import axios from 'axios'
 
-const instance = axios.create({})
+declare module 'axios' {
+  /**
+   * NOTE 拓展axios请求字段
+   */
+  interface AxiosRequestConfig {
+    /**
+     * 是否为外部请求
+     */
+    isExternalReq?: boolean
+    /**
+     * 是否为上传请求
+     */
+    isUploadReq?: boolean
+    /**
+     * 是否为下载请求
+     */
+    isDownloadReq?: boolean
+    /**
+     * 是否有前缀
+     */
+    noPrefix?: boolean
+    /**
+     * 是否为刷新请求
+     */
+    isRefreshToken?: boolean
+    /**
+     * 已重试次数(内部)
+     */
+    _retryCount?: number
+    /**
+     * 重试次数
+     */
+    retryTimes?: number
+    /**
+     * 重试间隔
+     */
+    retryDelay?: number
+  }
+}
+const instance = axios.create({
+  retryTimes: 3,
+  validateStatus(status) {
+    return status < 400
+  },
+})
 
 instance.interceptors.request.use(
   (config) => {
@@ -15,7 +59,7 @@ instance.interceptors.request.use(
     return config
   },
   (e) => {
-    console.error('req error', e)
+    console.log('request error', e)
   }
 )
 instance.interceptors.response.use(
@@ -25,9 +69,26 @@ instance.interceptors.response.use(
     if (status === 200) return data.data
     else return Promise.reject(data)
   },
-  (e) => {
-    console.error('res error', e)
+  (e: AxiosError) => {
+    console.log('response error: ', e.response?.status)
+    // 传递错误响应
+    return Promise.reject(e)
   }
 )
+
+instance.interceptors.response.use(null, (error: AxiosError) => {
+  const config = error.config
+  console.log(new Date().toLocaleTimeString(), config?.url, '_retryCount: ', config?._retryCount)
+  if (!config || !config.retryTimes) return Promise.reject(error)
+  const { _retryCount = 0, retryDelay = 1000, retryTimes } = config
+  config._retryCount = _retryCount
+
+  if (_retryCount >= retryTimes) return Promise.reject(error)
+
+  config._retryCount += 1
+  // 延时重试，延时时间随着重试次数增加
+  const delay = new Promise((resolve) => setTimeout(resolve, retryDelay * _retryCount))
+  return delay.then(() => instance(config))
+})
 
 export default instance
